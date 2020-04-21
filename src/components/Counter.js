ed141f483b51a270986cff3976c7f58b06464949
Zhaoyu <weixiaobo88@gmail.com>
2020-04-21 14:08:53 +0800

    [Linne] - add onIncrease handler for plus button

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,15 +4,25 @@ class Counter extends Component {
   constructor(props) {
     super(props);
 
+    this.onIncrease = this.onIncrease.bind(this);
+
     this.state = {
       number: 0,
     };
   }
 
+  onIncrease() {
+    this.setState((prevState) => {
+      return {
+        number: prevState.number + 1,
+      };
+    });
+  }
+
   render() {
     return (
       <div>
-        <button>+</button>
+        <button onClick={this.onIncrease}>+</button>
         <span>{this.state.number}</span>
         <button>-</button>
       </div>
