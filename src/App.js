42aa763838412cfbf388288e5f0836ff3e7fe6e7
Zhaoyu <weixiaobo88@gmail.com>
2020-04-21 13:47:26 +0800

    [Linne] - add Counter component with static code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello React
+        <Counter />
       </header>
     </div>
   );
