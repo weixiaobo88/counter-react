3c0d113fda8bc6febf036163e9762d5e3b5f9665
Zhaoyu <weixiaobo88@gmail.com>
2020-04-21 16:14:14 +0800

    [Linne] - generate counters with size by user input

--- src/components/CounterGroup.js
+++ src/components/CounterGroup.js
@@ -5,17 +5,30 @@ class CounterGroup extends Component {
   constructor(props) {
     super(props);
 
-    this.initArray = this.initArray.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.state = {
+      size: 2,
+    };
   }
 
   initArray(size) {
-    return Array.from(Array(size).keys());;
+    return Array.from(Array(size).keys());
+  }
+
+  onChange(event) {
+    const value = event.target.value;
+    this.setState({
+      size: value.length > 0 ? parseInt(value) : 0,
+    });
   }
 
   render() {
-    let counters = this.initArray(100);
+    let counters = this.initArray(this.state.size);
     return (
       <div>
+        <form>
+          <input onChange={this.onChange} type="text" value={this.state.size} />
+        </form>
         {counters.map((value) => (
           <Counter key={value} />
         ))}
